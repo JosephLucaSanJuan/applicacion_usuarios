9f607def38794751cf8f74b6cb53bf1d6c827c0a
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-05-04 13:22:03 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'applicacion_usuarios',
+  webDir: 'www'
+};
+
+export default config;
